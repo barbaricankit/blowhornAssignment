736f255a59068093196c37b3e3a6116fde60e046
unknown <ankit.singhania767@gmail.com>
2021-08-27 21:33:37 +0530

    refactor styling

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,14 +4,14 @@ const Header = () => {
   return (
     <div className="header">
       <h4 className="nav-option text-gray">nullBrains</h4>
-      <div>
+      <div className="nav-options">
         <span className="nav-option text--purple">Home</span>
         <span className="nav-option text-gray">Features</span>
         <span className="nav-option text-gray">Blog</span>
         <span className="nav-option text-gray">Contact</span>
         <BsSearch className="nav-option text-gray" />
       </div>
-      <div>
+      <div className="nav-options">
         <span className="nav-option text-gray">Sign In</span>
         <span className="nav-option btn--purple">Sign Up</span>
       </div>
